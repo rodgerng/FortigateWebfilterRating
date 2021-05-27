f037f1f6ce354758b6f476d1964b55009d9bef80
Rodger Ng Yong Fung <rodgerng@outlook.com>
2021-05-27 16:59:17 +0800

    updatedfile

--- WebFilterRatingv1.py
+++ WebFilterRatingv1.py
@@ -1,20 +1,20 @@
-print("Copyright 2021 created by Rodger Ng Yong Fung")
-setrating = input("Enter Rating ID = ")
 import sys
 import csv
+print("Copyright 2021 created by Rodger Ng Yong Fung")
+set_rating = input("Enter Rating ID = ")
 original_stdout = sys.stdout
 # Save a reference to the original standard output
 
 with open('output.txt', 'w') as f:
     sys.stdout = f
-# Change the standard output to the file we created.
+    # Change the standard output to the file we created.
     print("config webfilter ftgd-local-rating ")
     with open('blacklist.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
             print(f'edit "{" ".join(row)}"')
-            print("set rating " + setrating)
+            print("set rating " + set_rating)
             print("next")
             print("end")
             line_count += 1
